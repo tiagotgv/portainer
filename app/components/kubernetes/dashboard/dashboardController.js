12f743354e1573a5ca3b6b8b52a48d594e115599
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-01-22 21:18:08 +0100

    wip

---
+++ app/components/kubernetes/dashboard/dashboardController.js
@@ -0,0 +1,10 @@
+angular.module('portainer.kubernetes', [])
+.controller('KubernetesDashboardController', ['$scope', 'Notifications',
+function ($scope, Notifications) {
+
+  function initView() {
+
+  }
+
+  initView();
+}]);
